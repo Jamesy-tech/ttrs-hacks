743478907b1b5e07ffb8e6235f55f76c012d9f9c
jamesy-tech <jamesycreator@gmail.com>
2025-11-17 16:25:11 +1300

    Create get-answer.js

---
+++ get-answer.js
@@ -0,0 +1,25 @@
+javascript:(() => {
+
+    const num1El = document.querySelector('.mx-1.ng-star-inserted[data-qa-left]');
+    const num2El = document.querySelector('.mx-1.ng-star-inserted[data-qa-right]');
+    const operatorEl = document.querySelector('.mx-1.ng-star-inserted[data-qa-operator]');
+    const inputEl = document.querySelector('.hint-text');
+
+    const n1 = Number(num1El.textContent.trim());
+    const n2 = Number(num2El.textContent.trim());
+    const op = operatorEl.textContent.trim();
+
+    let answer = 0;
+    if (op === '×') {
+        answer = n1 * n2;
+    } else if (op === '÷') {
+        answer = n1 / n2;
+    } else if (op === '+') {
+        answer = n1 + n2;
+    } else if (op === '-') {
+        answer = n1 - n2;
+    }
+
+      alert(answer);
+
+})();
